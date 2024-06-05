fa4d28670fa7852a568a07bab1a661cab75e67bb
Arthur Fortes <arthurfortesdasilvaoficial@gmail.com>
2024-06-05 00:28:17 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay:true,
+        arrows: false
+    });
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required:true
+            },
+            email: {
+                required:true,
+                email:true
+            },
+            telefone: {
+                required:true
+            },
+            cpf: {
+                required:true
+            },
+            endereco: {
+                required:true
+            },
+            cep: {
+                required:true
+            }
+
+
+        }
+    })
+});
